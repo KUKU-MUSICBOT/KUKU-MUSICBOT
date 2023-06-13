373044860fd9180cc5c4e92680b7c8387c7fd03b
AN3H <136014659+KUKU-MUSICBOT@users.noreply.github.com>
2023-06-13 10:21:27 +0530

    Update userbot.py

--- VipX/core/userbot.py
+++ VipX/core/userbot.py
@@ -48,7 +48,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BEST_FRIENDS_CHAT_GROUP")
+                await self.one.join_chat("WTF_FCC")
             except:
                 pass
             assistants.append(1)
@@ -78,7 +78,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BEST_FRIENDS_CHAT_GROUP")
+                await self.two.join_chat("WTF_FCC")
             except:
                 pass
             assistants.append(2)
@@ -108,7 +108,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BEST_FRIENDS_CHAT_GROUP")
+                await self.three.join_chat("WTF_FCC")
             except:
                 pass
             assistants.append(3)
@@ -138,7 +138,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BEST_FRIENDS_CHAT_GROUP")
+                await self.four.join_chat("WTF_FCC")
             except:
                 pass
             assistants.append(4)
@@ -168,7 +168,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BEST_FRIENDS_CHAT_GROUP")
+                await self.five.join_chat("WTF_FCC")
             except:
                 pass
             assistants.append(5)
