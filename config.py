626a9c5bc42667c073f03fc1ddd4fa77ec03b661
AN3H <136014659+KUKU-MUSICBOT@users.noreply.github.com>
2023-07-06 09:30:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -130,7 +130,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(
 # Refer to https://i.postimg.cc/Bbg3LQTG/image.png
 SET_CMDS = getenv("SET_CMDS", False)
 
-# You'll need a Pyrogram String Session for these vars. Generate String from our session generator bot @YukkiStringBot
+# You'll need a Pyrogram String Session for these vars. Generate String from our session generator bot @ANSH_MAKERBOT
 STRING1 = getenv("STRING_SESSION", "")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
